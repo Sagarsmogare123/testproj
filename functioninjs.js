92546d664bc36f29a0f0244e166dae4a739973e4
sagar <59043146+Sagarsmogare123@users.noreply.github.com>
2022-08-16 15:05:21 +0530

    simple testing

---
+++ functioninjs.js
@@ -0,0 +1,37 @@
+// // Defining the function:
+// function sum(num1, num2) {
+//     let summ= num1 + num2;
+//     return summ;
+
+//   }
+
+//   let result= sum(3, 6); // 9
+
+//   console.log(result);
+
+
+//   // Calling the function:
+
+
+
+// var arr=[4,3,2,1]; 
+// var rev=arr.reverse(); 
+// // document.writeln(rev);
+// console.log(rev);
+
+// var values=["Three","two","one"]; varans=values.shift(); 
+// // document.writeln(ans);
+// console.log(ans);
+
+
+
+// var string1 = "Fee";
+//  var intvalue = 10000;
+// alert( string1 + intvalue );
+
+
+const obj1 = {
+    first: 20,
+    second: 30,
+    first: 50
+}; console.log(obj1);
